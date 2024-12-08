43fc8b2d3b7c83bcb7e71737088d94cc982152e1
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2024-12-08 16:55:06 +0100

    feat: SQLite store

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import json
+import sqlite3
 
 import spotipy
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# === SPOTIFY FETCH ===
 sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(
                     client_id=os.environ.get("SPOTIFY_CLIENT_ID"),
                     client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET"),
@@ -17,9 +19,63 @@ sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(
 # but we can never get more than 50 most recent
 results = sp.current_user_recently_played()
 
-with open("results.json", "w") as f:
-    f.write(json.dumps(results, indent=2, ensure_ascii=False))
+# ms since epoch -- time when the oldest song was played
+cutoff: int = int(results["cursors"]["before"])
+
+# === MANUAL RESULTS PROCESSING ===
+# with open("results.json", "w") as f:
+#     f.write(json.dumps(results, indent=2, ensure_ascii=False))
 
 summary: list[str] = [str((x["track"]["name"], x["played_at"])) for x in results["items"]]
 
 print("\n".join(summary))
+
+# === DB WRITE ===
+conn = sqlite3.connect("data.db")
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS played (
+    song_name TEXT,
+    song_id TEXT,
+    artist_name TEXT,
+    artist_id TEXT,
+    album_name TEXT,
+    album_id TEXT,
+    song_duration_ms INTEGER,
+    played_at TEXT,
+    playlist_uri TEXT
+);""")
+
+for item in results['items']:
+    track = item['track']
+    context = item.get('context', {})
+
+    song_name = track['name']
+    song_id = track['id']
+    artist_name = track['artists'][0]['name']
+    artist_id = track['artists'][0]['id']
+    album_name = track['album']['name']
+    album_id = track['album']['id']
+    song_duration_ms = track['duration_ms']
+    played_at = item['played_at']
+    playlist_uri = None
+
+    if context and context.get("type") == "playlist":
+        playlist_uri = context.get('uri', None)
+
+    cur.execute("""
+        INSERT INTO played (
+            song_name, song_id, artist_name, artist_id, album_name, album_id,
+            song_duration_ms, played_at, playlist_uri
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """, (song_name, song_id, artist_name, artist_id, album_name, album_id,
+            song_duration_ms, played_at, playlist_uri))
+
+conn.commit()
+
+# def list_all_names():
+
+cur.execute("SELECT song_name FROM played")
+rows = cur.fetchall()
+print(rows)
+
+conn.close()
