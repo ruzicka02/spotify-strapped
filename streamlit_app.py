28fdc57fc36e3bf4bfd76a254f972d5710a9a90a
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2024-12-10 20:56:53 +0100

    feat: Isolated streamlit app file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+from db import db_connect
+from queries import QUERY_FETCH_PLAY_COUNT
+
 def app_header():
     st.set_page_config(page_title="Spotify Strapped", page_icon="🎶")
     st.title("Spotify Strapped DEVEL")
@@ -7,3 +10,12 @@ def app_header():
 def print_table(table: list[tuple[str]]):
     markdown = "Song | Artist | Played\n-|-|-\n" + "\n".join(["|".join([str(x) for x in row]) for row in table])
     st.markdown(markdown)
+
+if __name__ == "__main__":
+    app_header()
+
+    conn, cur = db_connect()
+    cur.execute(QUERY_FETCH_PLAY_COUNT)
+    names = cur.fetchall()
+
+    print_table(names)
